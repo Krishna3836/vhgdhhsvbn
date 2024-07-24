ea94cedf44b8f293f421eb8735a6329189428765
Krishna <96517910+NrTeams@users.noreply.github.com>
2024-07-24 18:18:25 +0530

    Update base.py

--- bot/base.py
+++ bot/base.py
@@ -1,4 +1,4 @@
-from bot.__init__ import create_client
+#from bot.__init__ import create_client
 from bot.config import TG_CONFIG
 import pyrogram
 from pyrogram import Client as client
@@ -11,13 +11,3 @@ logger = logging.getLogger(__name__)
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from bot import create_client
-
-client = create_client(TG_CONFIG.stringhi)
-if client:
-    IS_PREMIUM_USER = client.me.is_premium
-    logging.info("Bot started successfully")
-    # Use the client object here
-    print("Client object:", client)
-else:
-    logging.error("Failed to create client")
